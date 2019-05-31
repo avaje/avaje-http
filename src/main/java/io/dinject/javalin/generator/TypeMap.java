ed2fd9c98086cd112d8e826f7a7e30194b9a8f54
rob bygrave <robin.bygrave@gmail.com>
2019-06-01 01:07:32 +1200

    #9 - Add type conversion for boolean, Boolean, BigDecimal, Instant, OffsetDateTime

--- src/main/java/io/dinject/javalin/generator/TypeMap.java
+++ src/main/java/io/dinject/javalin/generator/TypeMap.java
@@ -17,20 +17,30 @@ class TypeMap {
   }
 
   static {
-    types.put("int", new IntHander());
-    types.put("long", new LongHander());
-    types.put("java.lang.String", new StringHander());
-    types.put("java.lang.Integer", new IntegerHander());
+    types.put("int", new IntHandler());
+    types.put("long", new PLongHandler());
+    types.put("boolean", new BoolHandler());
+
+    types.put("java.lang.String", new StringHandler());
+    types.put("java.lang.Integer", new IntegerHandler());
+    types.put("java.lang.Long", new LongHander());
+    types.put("java.lang.Boolean", new BooleanHandler());
+
     add(new UuidHandler());
+    add(new BigDecimalHandler());
     add(new LocalDateHandler());
+    add(new LocalTimeHandler());
+    add(new LocalDateTimeHandler());
+    add(new InstantHandler());
+    add(new OffsetDateTimeHandler());
   }
 
   static TypeHandler get(String type) {
     return types.get(type);
   }
 
-  static class StringHander extends JavaLangType {
-    StringHander() {
+  static class StringHandler extends JavaLangType {
+    StringHandler() {
       super("String");
     }
 
@@ -45,8 +55,8 @@ class TypeMap {
     }
   }
 
-  static class IntegerHander extends JavaLangType {
-    IntegerHander() {
+  static class IntegerHandler extends JavaLangType {
+    IntegerHandler() {
       super("Integer");
     }
 
@@ -61,15 +71,53 @@ class TypeMap {
     }
   }
 
-  static class IntHander extends Primitive {
-    IntHander() {
+  static class IntHandler extends Primitive {
+    IntHandler() {
       super("Int");
     }
   }
 
-  static class LongHander extends Primitive {
+  static class LongHander extends JavaLangType {
     LongHander() {
       super("Long");
+    }
+
+    @Override
+    public String asMethod() {
+      return "asLong(";
+    }
+
+    @Override
+    public String toMethod() {
+      return "toLong(";
+    }
+  }
+
+  static class PLongHandler extends Primitive {
+    PLongHandler() {
+      super("Long");
+    }
+  }
+
+  static class BooleanHandler extends JavaLangType {
+    BooleanHandler() {
+      super("Boolean");
+    }
+
+    @Override
+    public String asMethod() {
+      return "asBool(";
+    }
+
+    @Override
+    public String toMethod() {
+      return "toBoolean(";
+    }
+  }
+
+  static class BoolHandler extends Primitive {
+    BoolHandler() {
+      super("asBool(", "boolean");
     }
   }
 
@@ -103,6 +151,11 @@ class TypeMap {
       this.type = asType.toLowerCase();
     }
 
+    Primitive(String asMethod, String type) {
+      this.asMethod = asMethod;
+      this.type = type;
+    }
+
     @Override
     public String shortName() {
       return type;
@@ -124,26 +177,57 @@ class TypeMap {
     }
   }
 
-  static class UuidHandler extends ObjectHander {
+  static class UuidHandler extends ObjectHandler {
     UuidHandler() {
       super("java.util.UUID", "UUID");
     }
   }
 
-  static class LocalDateHandler extends ObjectHander {
+  static class BigDecimalHandler extends ObjectHandler {
+    BigDecimalHandler() {
+      super("java.math.BigDecimal", "BigDecimal");
+    }
+  }
+
+  static class LocalDateHandler extends ObjectHandler {
     LocalDateHandler() {
       super("java.time.LocalDate", "LocalDate");
     }
   }
 
-  static abstract class ObjectHander implements TypeHandler {
+  static class InstantHandler extends ObjectHandler {
+    InstantHandler() {
+      super("java.time.Instant", "Instant");
+    }
+  }
+
+  static class OffsetDateTimeHandler extends ObjectHandler {
+    OffsetDateTimeHandler() {
+      super("java.time.OffsetDateTime", "OffsetDateTime");
+    }
+  }
+
+  static class LocalTimeHandler extends ObjectHandler {
+    LocalTimeHandler() {
+      super("java.time.LocalTime", "LocalTime");
+    }
+  }
+
+  static class LocalDateTimeHandler extends ObjectHandler {
+    LocalDateTimeHandler() {
+      super("java.time.LocalDateTime", "LocalDateTime");
+    }
+  }
+
+
+  static abstract class ObjectHandler implements TypeHandler {
 
     private final String importType;
     private final String shortName;
     private final String asMethod;
     private final String toMethod;
 
-    ObjectHander(String importType, String shortName) {
+    ObjectHandler(String importType, String shortName) {
       this.importType = importType;
       this.shortName = shortName;
       this.asMethod = "as" + shortName + "(";
