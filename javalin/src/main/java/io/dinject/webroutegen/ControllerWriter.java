03cf7c16c3501a201bf91c7a8daab0bf7a123cec
rob bygrave <robin.bygrave@gmail.com>
2020-07-02 16:45:54 +1200

    Refactor extract BaseControllerWriter

--- javalin/src/main/java/io/dinject/webroutegen/ControllerWriter.java
+++ javalin/src/main/java/io/dinject/webroutegen/ControllerWriter.java
@@ -1,48 +1,25 @@
 package io.dinject.webroutegen;
 
-import javax.lang.model.element.TypeElement;
-import javax.tools.JavaFileObject;
 import java.io.IOException;
-import java.io.Writer;
 
 /**
  * Write Javalin specific Controller WebRoute handling adapter.
  */
-class ControllerWriter {
+class ControllerWriter extends BaseControllerWriter {
 
   private static final String AT_GENERATED = "@Generated(\"io.dinject.javalin-webgen\")";
   private static final String API_BUILDER = "io.javalin.apibuilder.ApiBuilder";
 
-  private final ControllerReader reader;
-
-  private final ProcessingContext ctx;
-
-  private final String originName;
-  private final String shortName;
-  private final String fullName;
-  private String packageName;
-  private Append writer;
-
-  ControllerWriter(ControllerReader reader, ProcessingContext ctx) {
-    this.reader = reader;
-    this.ctx = ctx;
+  ControllerWriter(ControllerReader reader, ProcessingContext ctx) throws IOException {
+    super(reader, ctx);
     reader.addImportType(API_BUILDER);
-
-    TypeElement origin = reader.getBeanType();
-    originName = origin.getQualifiedName().toString();
-    shortName = origin.getSimpleName().toString();
-    fullName = originName + "$route";
   }
 
-  void write() throws IOException {
-
-    writer = new Append(createFileWriter());
+  void write() {
     writePackage();
     writeImports();
     writeClassStart();
-
     writeAddRoutes();
-
     writeClassEnd();
     writer.close();
   }
@@ -66,23 +43,6 @@ class ControllerWriter {
 
   private void writeForMethod(MethodReader method) {
     new ControllerMethodWriter(method, writer).write();
-  }
-
-  private void writeImports() {
-
-    writer.append(Constants.IMPORT_PATH_TYPE_CONVERT).eol();
-    for (String type : reader.getStaticImportTypes()) {
-      writer.append("import static %s;", type).eol();
-    }
-    writer.eol();
-    for (String type : reader.getImportTypes()) {
-      writer.append("import %s;", type).eol();
-    }
-    writer.eol();
-  }
-
-  private void writeClassEnd() {
-    writer.append("}").eol();
   }
 
   private void writeClassStart() {
@@ -110,19 +70,4 @@ class ControllerWriter {
     writer.append(" }").eol().eol();
   }
 
-  private void writePackage() {
-    if (packageName != null) {
-      writer.append("package %s;", packageName).eol().eol();
-    }
-  }
-
-  private Writer createFileWriter() throws IOException {
-    int dp = originName.lastIndexOf('.');
-    if (dp > -1) {
-      packageName = originName.substring(0, dp);
-    }
-
-    JavaFileObject jfo = ctx.createWriter(fullName, reader.getBeanType());
-    return jfo.openWriter();
-  }
 }
