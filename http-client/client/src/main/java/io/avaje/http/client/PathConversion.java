ecf5c981d9de3fba351afb0d9e0f7d6564c44d58
rbygrave <robin.bygrave@gmail.com>
2021-06-21 12:32:53 +1200

    Simplify PathConversion removing unnecessary methods

--- http-client/client/src/main/java/io/avaje/http/client/PathConversion.java
+++ http-client/client/src/main/java/io/avaje/http/client/PathConversion.java
@@ -1,9 +1,5 @@
 package io.avaje.http.client;
 
-import java.math.BigDecimal;
-import java.time.*;
-import java.util.UUID;
-
 /**
  * Helper methods to convert common types to String path values.
  */
@@ -33,69 +29,6 @@ public class PathConversion {
   /**
    * Convert to path.
    */
-  public static String toPath(UUID val) {
-    return val.toString();
-  }
-
-  /**
-   * Convert to path.
-   */
-  public static String toPath(LocalDate val) {
-    return val.toString();
-  }
-
-  /**
-   * Convert to path.
-   */
-  public static String toPath(LocalTime val) {
-    return val.toString();
-  }
-
-  /**
-   * Convert to path.
-   */
-  public static String toPath(LocalDateTime val) {
-    return val.toString();
-  }
-
-  /**
-   * Convert to path.
-   */
-  public static String toPath(Instant val) {
-    return val.toString();
-  }
-
-  /**
-   * Convert to path.
-   */
-  public static String toPath(OffsetDateTime val) {
-    return val.toString();
-  }
-
-  /**
-   * Convert to path.
-   */
-  public static String toPath(ZonedDateTime val) {
-    return val.toString();
-  }
-
-  /**
-   * Convert to path.
-   */
-  public static String toPath(ZoneId val) {
-    return val.toString();
-  }
-
-  /**
-   * Convert to path.
-   */
-  public static String toPath(BigDecimal val) {
-    return val.toString();
-  }
-
-  /**
-   * Convert to path.
-   */
   public static String toPath(double val) {
     return Double.toString(val);
   }
@@ -105,6 +38,13 @@ public class PathConversion {
    */
   public static String toPath(float val) {
     return Float.toString(val);
+  }
+
+  /**
+   * Convert to path.
+   */
+  public static String toPath(Object val) {
+    return val.toString();
   }
 
 }
