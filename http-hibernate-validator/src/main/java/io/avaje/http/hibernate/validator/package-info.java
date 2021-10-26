ffc4ef0c675f7a94662f6c14a87bdeb449d362a9
Rob Bygrave <robin.bygrave@gmail.com>
2021-10-26 14:25:50 +1300

    Bump to latest avaje-inject

--- http-hibernate-validator/src/main/java/io/avaje/http/hibernate/validator/package-info.java
+++ http-hibernate-validator/src/main/java/io/avaje/http/hibernate/validator/package-info.java
@@ -1,4 +1,4 @@
-@InjectModule(name = "bean-validator", provides = "validator")
+@InjectModule(name = "bean-validator", provides = io.avaje.http.api.Validator.class)
 package io.avaje.http.hibernate.validator;
 
 import io.avaje.inject.InjectModule;
