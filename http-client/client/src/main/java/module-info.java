471be61b2172ecf0501023cb00f3bd1488e352f4
Rob Bygrave <robin.bygrave@gmail.com>
2022-02-25 13:58:30 +1300

    Update module-info to optionally use jackson annotation and core

--- http-client/client/src/main/java/module-info.java
+++ http-client/client/src/main/java/module-info.java
@@ -5,6 +5,8 @@ module io.avaje.http.client {
   requires transitive java.net.http;
   requires transitive org.slf4j;
   requires static com.fasterxml.jackson.databind;
+  requires static com.fasterxml.jackson.annotation;
+  requires static com.fasterxml.jackson.core;
   requires static io.avaje.jsonb;
 
   exports io.avaje.http.client;
