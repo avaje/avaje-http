46095c730f3bac00ac3aabc43c3d68ee10057889
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2022-10-15 19:43:53 -0400

    enabled jsonb nima test

--- tests/test-nima-jsonb/src/main/java/org/example/Main.java
+++ tests/test-nima-jsonb/src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import java.util.List;
 
 import io.avaje.inject.BeanScope;
+import io.avaje.jsonb.Jsonb;
 import io.helidon.nima.webserver.WebServer;
 import io.helidon.nima.webserver.http.HttpRouting;
 import io.helidon.nima.webserver.http.HttpService;
@@ -11,7 +12,7 @@ public class Main {
 
   public static void main(String[] args) {
 
-    final var scope = BeanScope.builder().build();
+    final var scope = BeanScope.builder().beans(Jsonb.builder().build()).build();
     final List<HttpService> list = scope.list(HttpService.class);
     final var builder = HttpRouting.builder();
     for (final HttpService httpService : list) {
@@ -19,12 +20,11 @@ public class Main {
     }
     final var httpRouting = builder.build();
 
-
     WebServer.builder()
-      .addRouting(httpRouting)
-      //.routing(Main::routing)
-      .port(8081)
-      .start();
+        .addRouting(httpRouting)
+        // .routing(Main::routing)
+        .port(8081)
+        .start();
 
     System.out.println("started");
   }
