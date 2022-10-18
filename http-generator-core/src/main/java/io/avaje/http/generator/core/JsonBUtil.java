bac1d650e428c2aa63b7ec8a58ec8f7c306b8db1
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2022-10-18 17:49:17 -0400

    unstatic jsontype and primitive map

--- http-generator-core/src/main/java/io/avaje/http/generator/core/JsonBUtil.java
+++ http-generator-core/src/main/java/io/avaje/http/generator/core/JsonBUtil.java
@@ -2,37 +2,38 @@ package io.avaje.http.generator.core;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class JsonBUtil {
   private JsonBUtil() {}
 
-  private static final Map<String, UType> jsonTypes = new LinkedHashMap<>();
-
   public static Map<String, UType> getJsonTypes(ControllerReader reader) {
+
+    final Map<String, UType> jsonTypes = new LinkedHashMap<>();
+
+    final Consumer<UType> addToMap = uType -> jsonTypes.put(uType.full(), uType);
+
     reader.getMethods().stream()
         .filter(MethodReader::isWebMethod)
         .filter(m -> !"byte[]".equals(m.getReturnType().toString()))
         .filter(m -> m.getProduces() == null || m.getProduces().toLowerCase().contains("json"))
         .forEach(
             methodReader -> {
-              addJsonBodyType(methodReader);
+              addJsonBodyType(methodReader, addToMap);
               if (!methodReader.isVoid()) {
-                addJsonType(UType.parse(methodReader.getReturnType()));
+                addToMap.accept(UType.parse(methodReader.getReturnType()));
               }
             });
 
     return Map.copyOf(jsonTypes);
   }
 
-  private static void addJsonType(UType type) {
-    jsonTypes.put(type.full(), type);
-  }
-
-  private static void addJsonBodyType(MethodReader methodReader) {
+  private static void addJsonBodyType(MethodReader methodReader, Consumer<UType> addToMap) {
     if (methodReader.getBodyType() != null) {
       methodReader.getParams().stream()
           .filter(MethodParam::isBody)
-          .forEach(param -> addJsonType(param.getUType()));
+          .map(MethodParam::getUType)
+          .forEach(addToMap);
     }
   }
 
--- http-generator-core/src/main/java/io/avaje/http/generator/core/PrimitiveUtil.java
+++ http-generator-core/src/main/java/io/avaje/http/generator/core/PrimitiveUtil.java
@@ -1,24 +1,21 @@
 package io.avaje.http.generator.core;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public final class PrimitiveUtil {
 
   private PrimitiveUtil() {}
 
-  static final Map<String, String> wrapperMap = new HashMap<>();
-
-  static {
-    wrapperMap.put("char", "Character");
-    wrapperMap.put("byte", "Byte");
-    wrapperMap.put("int", "Integer");
-    wrapperMap.put("long", "Long");
-    wrapperMap.put("short", "Short");
-    wrapperMap.put("double", "Double");
-    wrapperMap.put("float", "Float");
-    wrapperMap.put("boolean", "Boolean");
-  }
+  static final Map<String, String> wrapperMap =
+      Map.of(
+          "char", "Character",
+          "byte", "Byte",
+          "int", "Integer",
+          "long", "Long",
+          "short", "Short",
+          "double", "Double",
+          "float", "Float",
+          "boolean", "Boolean");
 
   public static String wrap(String shortName) {
     final var wrapped = wrapperMap.get(shortName);
