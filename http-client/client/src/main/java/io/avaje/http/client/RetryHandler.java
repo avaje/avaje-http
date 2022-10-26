5129caa0df48e2c070935da03f4193c935a80e60
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2022-10-25 21:45:45 -0400

    add retry exception handler

--- http-client/client/src/main/java/io/avaje/http/client/RetryHandler.java
+++ http-client/client/src/main/java/io/avaje/http/client/RetryHandler.java
@@ -11,9 +11,19 @@ public interface RetryHandler {
    * Return true if the request should be retried.
    *
    * @param retryCount The number of retry attempts already executed
-   * @param response   The HTTP response
+   * @param response The HTTP response
    * @return True if the request should be retried or false if not
    */
   boolean isRetry(int retryCount, HttpResponse<?> response);
 
+  /**
+   * Return true if the request should be retried.
+   *
+   * @param retryCount The number of retry attempts already executed
+   * @param exception The Wrapped Error thrown by the underlying Http Client
+   * @return True if the request should be retried or false if not
+   */
+  default boolean isExceptionRetry(int retryCount, HttpException exception) {
+    throw exception;
+  }
 }
