6efb448b6c31a0096b15b7956107cf8c9b51cf37
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2022-12-25 20:54:11 -0500

    Update OpenAPIReturnsContainer.java

--- http-api/src/main/java/io/avaje/http/api/OpenAPIReturnsContainer.java
+++ http-api/src/main/java/io/avaje/http/api/OpenAPIReturnsContainer.java
@@ -6,6 +6,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/** a container for the OpenAPIReturns annotation to make it repeatable */
 @Target(value = METHOD)
 @Retention(value = RUNTIME)
 public @interface OpenAPIReturnsContainer {
