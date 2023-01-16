d0c19d208bb1cf78ed6e565ca8d67963cd0845f0
Rob Bygrave <robin.bygrave@gmail.com>
2023-01-16 21:57:06 +1300

    #125 - Add unit tests for Util.trimAnnotations()

--- http-generator-core/src/test/java/io/avaje/http/generator/core/UtilTest.java
+++ http-generator-core/src/test/java/io/avaje/http/generator/core/UtilTest.java
@@ -186,4 +186,12 @@ public class UtilTest {
     assertThat(Util.name("Map<String,Person>")).isEqualTo("mapStringPerson");
   }
 
+  @Test
+  void trimAnnotations() {
+    assertEquals("java.lang.String", Util.trimAnnotations("java.lang.String"));
+    assertEquals("java.lang.String", Util.trimAnnotations("java.lang.@javax.validation.constraints.Email String"));
+    assertEquals("java.lang.String", Util.trimAnnotations("java.lang.@javax.validation.constraints.NotNull,@javax.validation.constraints.Size(min=2, max=150) String"));
+    assertEquals("java.lang.String", Util.trimAnnotations("java.lang.@javax.validation.constraints.Email,@javax.validation.constraints.Size(max=100) String"));
+  }
+
 }
