e43a596d5bf4fb7c3dde44d024f3e958d21efcd4
Rob Bygrave <robin.bygrave@gmail.com>
2023-03-20 15:51:45 +1300

    Fix #188 - IllegalAccessError io.avaje.http.generator.core.openapi.KnownTypes ... does not read module java.sql (#190)

--- http-generator-client/src/main/java/module-info.java
+++ http-generator-client/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module io.avaje.http.client.generator {
   provides javax.annotation.processing.Processor with io.avaje.http.generator.client.ClientProcessor;
 
   requires java.compiler;
+  requires java.sql;
 
   // SHADED: All content after this line will be removed at package time
   requires transitive io.avaje.http.generator.core;
--- http-generator-helidon/src/main/java/module-info.java
+++ http-generator-helidon/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module io.avaje.http.helidon.generator {
   provides javax.annotation.processing.Processor with io.avaje.http.generator.helidon.HelidonProcessor;
 
   requires java.compiler;
+  requires java.sql;
 
   // SHADED: All content after this line will be removed at package time
   requires transitive io.avaje.http.generator.core;
