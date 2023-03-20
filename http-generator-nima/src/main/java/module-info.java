867c5fee8412df8192edd07926ecbf8d8e1e0bd5
Rob Bygrave <robin.bygrave@gmail.com>
2023-03-20 15:25:51 +1300

    Fix #188 - IllegalAccessError io.avaje.http.generator.core.openapi.KnownTypes ... does not read module java.sql

--- http-generator-nima/src/main/java/module-info.java
+++ http-generator-nima/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module io.avaje.http.nima.generator {
   provides javax.annotation.processing.Processor with io.avaje.http.generator.helidon.nima.NimaProcessor;
 
   requires java.compiler;
+  requires java.sql;
 
   // SHADED: All content after this line will be removed at package time
   requires transitive io.avaje.http.generator.core;
