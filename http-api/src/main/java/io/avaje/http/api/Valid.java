d712402dd1c7a6adf6331cbf0f2c5fa057b8b6ad
Rob Bygrave <robin.bygrave@gmail.com>
2023-04-04 09:37:05 +1200

    Javadoc on api Valid annotation

--- http-api/src/main/java/io/avaje/http/api/Valid.java
+++ http-api/src/main/java/io/avaje/http/api/Valid.java
@@ -9,15 +9,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- *
- * Add @Valid annotation on a controller/method/BeanParam that we want bean validation to be included for.
- * When we do this controller methods that take a request payload will then have the request bean
- * (populated by JSON payload or form parameters) validated before it is passed to the controller
- * method.
- *
- * When trying to validate a @BeanParam bean, this will need to be placed on the BeanParam type
-
+ * Add {@code @Valid} annotation on a controller/method/BeanParam that we want bean validation to
+ * be included for. When we do this controller methods that take a request payload will then have
+ * the request bean (populated by JSON payload or form parameters) validated before it is passed
+ * to the controller method.
+ * <p>
+ * When trying to validate a {@code @BeanParam} bean, this will need to be placed on the BeanParam type.
+ * <p>
+ * When using this annotation we need to provide an implementation of {@link Validator} to use.
+ * <p>
+ * Alternatively we can use the Jakarta {@code @Valid} along with a Jakarta validator implementation.
  */
 @Retention(SOURCE)
 @Target({METHOD, TYPE, PARAMETER})
-public @interface Valid {}
+public @interface Valid {
+}
