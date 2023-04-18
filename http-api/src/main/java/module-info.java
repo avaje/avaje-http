daebcd81014c8ce4b20af837a08b47b336d7d43a
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-04-18 10:32:52 -0400

    Update module-info.java

--- http-api/src/main/java/module-info.java
+++ http-api/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module io.avaje.http.api {
 
 	exports io.avaje.http.api;
 	exports io.avaje.http.api.context;
-	exports io.avaje.http.api.spi;
 	requires static io.avaje.inject;
 
 	provides io.avaje.inject.spi.Plugin with io.avaje.http.api.spi.DefaultResolverProvider;
