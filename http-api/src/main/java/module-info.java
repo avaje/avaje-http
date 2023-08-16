4bfbcbeba225a4445ebed1eb2d3652f80c9e2469
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-08-15 23:56:26 -0400

    move javalin annotations out of generator

--- http-api/src/main/java/module-info.java
+++ http-api/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module io.avaje.http.api {
 
 	exports io.avaje.http.api;
+	exports io.avaje.http.api.javalin;
 	exports io.avaje.http.api.context;
 	exports io.avaje.http.api.spi;
 }
--- http-generator-javalin/src/main/java/module-info.java
+++ http-generator-javalin/src/main/java/module-info.java
@@ -6,6 +6,7 @@ module io.avaje.http.javalin.generator {
   requires java.sql;
 
   // SHADED: All content after this line will be removed at package time
-  requires transitive io.avaje.http.generator.core;
+  requires io.avaje.http.generator.core;
+  requires io.avaje.http.api;
   requires static io.avaje.prism;
 }
