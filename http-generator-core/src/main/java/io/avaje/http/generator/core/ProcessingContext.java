2874868628eb6f98d0fa0a73a3cbdbebb9f93af3
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-02-26 20:28:11 -0500

    Update ProcessingContext.java

--- http-generator-core/src/main/java/io/avaje/http/generator/core/ProcessingContext.java
+++ http-generator-core/src/main/java/io/avaje/http/generator/core/ProcessingContext.java
@@ -1,7 +1,5 @@
 package io.avaje.http.generator.core;
 
-import static io.avaje.jsonb.generator.APContext.filer;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
