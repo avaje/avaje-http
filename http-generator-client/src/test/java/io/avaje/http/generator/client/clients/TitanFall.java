7800cb8d326881e77b068a93f47050fda6d502f6
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-03-14 20:24:42 -0400

    add Headers Annotation

--- http-generator-client/src/test/java/io/avaje/http/generator/client/clients/TitanFall.java
+++ http-generator-client/src/test/java/io/avaje/http/generator/client/clients/TitanFall.java
@@ -2,14 +2,18 @@ package io.avaje.http.generator.client.clients;
 
 import io.avaje.http.api.Client;
 import io.avaje.http.api.Get;
+import io.avaje.http.api.Headers;
 
 @Client
+@Headers("Content-Type: applicaton/json")
 public interface TitanFall {
 
   @Get("/${titan}/${drop.point}")
+  @Headers("Something: \\invalid\n\t")
   Titan titanFall();
 
 
   @Get("/${titan}/copium")
+  @Headers("      Accept    :   applicaton/json")
   Titan titanFall3();
 }
