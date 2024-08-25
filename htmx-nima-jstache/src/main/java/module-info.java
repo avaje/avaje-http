4619fa6320a0b300f0b04dc6ef5789d06dc37e8e
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-08-25 16:06:32 -0400

    fix module

--- htmx-nima-jstache/src/main/java/module-info.java
+++ htmx-nima-jstache/src/main/java/module-info.java
@@ -5,7 +5,7 @@ module io.avaje.htmx.nima.jstache {
   requires transitive io.avaje.htmx.nima;
   requires transitive io.helidon.webserver;
   requires transitive io.jstach.jstachio;
-  requires io.avaje.inject;
+  requires transitive io.avaje.inject;
 
-  provides io.avaje.inject.spi.Plugin with io.avaje.htmx.nima.jstache.DefaultTemplateProvider;
+  provides io.avaje.inject.spi.InjectPlugin with io.avaje.htmx.nima.jstache.DefaultTemplateProvider;
 }
