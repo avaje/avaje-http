c5e24ed2dddb4fa73dd129e21c6e80ab51f7cb23
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-01-14 21:28:28 -0500

    [client] write clients to the same package
    also make then final

--- http-generator-client/src/main/java/io/avaje/http/generator/client/ClientWriter.java
+++ http-generator-client/src/main/java/io/avaje/http/generator/client/ClientWriter.java
@@ -37,7 +37,7 @@ final class ClientWriter extends BaseControllerWriter {
   @Override
   protected String initPackageName(String originName) {
     // put the generated Http client into a sub-package
-    return super.initPackageName(originName) + ".httpclient";
+    return super.initPackageName(originName);
   }
 
   private void readMethods() {
@@ -73,7 +73,7 @@ final class ClientWriter extends BaseControllerWriter {
     writer.append(AT_GENERATED).eol();
     AnnotationUtil.writeAnnotations(writer, reader.beanType());
 
-    writer.append("public class %s%s implements %s, AutoCloseable {", shortName, suffix, shortName).eol().eol();
+    writer.append("public final class %s%s implements %s, AutoCloseable {", shortName, suffix, shortName).eol().eol();
 
     writer.append("  private final HttpClient client;").eol().eol();
 
