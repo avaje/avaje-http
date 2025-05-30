9003f348c665e94be75889ae2cbaa970fcd7bf45
Rob Bygrave <robin.bygrave@gmail.com>
2025-05-30 20:33:47 +1200

    [helidon] Add test only for list of query parameters
    One or multiple params passes. Adding a test for NO parameters will fail.

---
+++ tests/test-nima/src/test/java/org/example/HelloControllerTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import java.net.http.HttpResponse;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HelloControllerTest extends BaseWebTest {
+
+  @Test
+  void listParamOne() {
+    HttpResponse<String> res = client().request()
+      .path("listParams")
+      .queryParam("codes", "123")
+      .GET()
+      .asPlainString();
+
+    assertThat(res.statusCode()).isEqualTo(200);
+    assertThat(res.body()).isEqualTo("codes:[123]");
+  }
+
+  @Test
+  void listParamMultiple() {
+    HttpResponse<String> res = client().request()
+      .path("listParams")
+      .queryParam("codes", "123")
+      .queryParam("codes", "456")
+      .GET()
+      .asPlainString();
+
+    assertThat(res.statusCode()).isEqualTo(200);
+    assertThat(res.body()).isEqualTo("codes:[123, 456]");
+  }
+}
