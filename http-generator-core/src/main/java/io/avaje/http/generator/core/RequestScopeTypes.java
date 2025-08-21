1a8594b34b59e094e4c97ca2995ba86afe8f5fca
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-08-21 00:47:40 -0400

    support jex request scope

--- http-generator-core/src/main/java/io/avaje/http/generator/core/RequestScopeTypes.java
+++ http-generator-core/src/main/java/io/avaje/http/generator/core/RequestScopeTypes.java
@@ -1,24 +1,18 @@
 package io.avaje.http.generator.core;
 
-import java.util.HashSet;
 import java.util.Set;
 
 class RequestScopeTypes {
 
   private static final String JAVALIN_CONTEXT = "io.javalin.http.Context";
+  private static final String JEX_CONTEXT = "io.avaje.jex.http.Context";
   private static final String HELIDON_REQ = "io.helidon.webserver.ServerRequest";
   private static final String HELIDON_RES = "io.helidon.webserver.ServerResponse";
-  private static final Set<String> TYPES = new HashSet<>();
 
-  static {
-    TYPES.add(JAVALIN_CONTEXT);
-    TYPES.add(HELIDON_REQ);
-    TYPES.add(HELIDON_RES);
-  }
+  private static final Set<String> TYPES =
+      Set.of(JAVALIN_CONTEXT, JEX_CONTEXT, HELIDON_REQ, HELIDON_RES);
 
-  /**
-   * Return true if the type is a request scoped type.
-   */
+  /** Return true if the type is a request scoped type. */
   static boolean isRequestType(String rawType) {
     return TYPES.contains(rawType);
   }
