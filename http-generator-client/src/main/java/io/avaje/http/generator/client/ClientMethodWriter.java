922e4baf79338a5dca6756f6ec943d1d5861bfaf
Rob Bygrave <robin.bygrave@gmail.com>
2025-09-02 08:47:59 +1200

    Formatting only

--- http-generator-client/src/main/java/io/avaje/http/generator/client/ClientMethodWriter.java
+++ http-generator-client/src/main/java/io/avaje/http/generator/client/ClientMethodWriter.java
@@ -69,8 +69,8 @@ final class ClientMethodWriter {
     this.timeout = method.timeout();
     this.useConfig = ProcessingContext.typeElement("io.avaje.config.Config") != null;
     this.suppressLogging =
-        SuppressLoggingPrism.isPresent(method.element())
-            || SuppressLoggingPrism.isPresent(method.element().getEnclosingElement());
+      SuppressLoggingPrism.isPresent(method.element())
+        || SuppressLoggingPrism.isPresent(method.element().getEnclosingElement());
     this.segmentPropertyMap =
       method.pathSegments().segments().stream()
         .filter(Segment::isProperty)
