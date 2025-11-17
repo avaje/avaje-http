ccda24d2ff14af8d6358ea0e122aa4956c83711b
Rob Bygrave <robin.bygrave@gmail.com>
2025-11-17 20:22:44 +1300

    #661 Fix Helidon request scope - ServerRequest / ServerResponse are injected into controller
    The issue is in RequestScopeTypes where the fully qualified name of Helidon
    request and response types have moved into a different package.

--- http-generator-core/src/main/java/io/avaje/http/generator/core/RequestScopeTypes.java
+++ http-generator-core/src/main/java/io/avaje/http/generator/core/RequestScopeTypes.java
@@ -6,11 +6,13 @@ class RequestScopeTypes {
 
   private static final String JAVALIN_CONTEXT = "io.javalin.http.Context";
   private static final String JEX_CONTEXT = "io.avaje.jex.http.Context";
-  private static final String HELIDON_REQ = "io.helidon.webserver.ServerRequest";
-  private static final String HELIDON_RES = "io.helidon.webserver.ServerResponse";
+  private static final String HELIDON0_REQ = "io.helidon.webserver.ServerRequest";
+  private static final String HELIDON0_RES = "io.helidon.webserver.ServerResponse";
+  private static final String HELIDON1_REQ = "io.helidon.webserver.http.ServerRequest";
+  private static final String HELIDON1_RES = "io.helidon.webserver.http.ServerResponse";
 
   private static final Set<String> TYPES =
-      Set.of(JAVALIN_CONTEXT, JEX_CONTEXT, HELIDON_REQ, HELIDON_RES);
+      Set.of(JAVALIN_CONTEXT, JEX_CONTEXT, HELIDON0_REQ, HELIDON0_RES, HELIDON1_REQ, HELIDON1_RES);
 
   /** Return true if the type is a request scoped type. */
   static boolean isRequestType(String rawType) {
