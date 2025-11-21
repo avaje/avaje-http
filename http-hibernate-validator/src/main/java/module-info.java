0fa0ffc003545acb20e10acabcf648dce681ce58
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-11-21 12:11:22 -0500

    add hibernate validator to build
    We finally hit the version number to add this to the regular build

--- http-hibernate-validator/src/main/java/module-info.java
+++ http-hibernate-validator/src/main/java/module-info.java
@@ -2,11 +2,9 @@ import io.avaje.http.hibernate.validator.ValidatorProvider;
 
 module io.avaje.http.hibernate.validator {
 
-  exports io.avaje.http.hibernate.validator;
-
-  requires io.avaje.http.api;
-  requires io.avaje.inject;
-  requires jakarta.validation;
+  requires transitive io.avaje.http.api;
+  requires transitive io.avaje.inject;
+  requires transitive jakarta.validation;
 
   provides io.avaje.inject.spi.InjectExtension with ValidatorProvider;
 }
