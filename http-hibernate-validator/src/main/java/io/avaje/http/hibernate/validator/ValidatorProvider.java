fa0b6dec0ae76a72e8151577afe07fd1a52537f1
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-11-21 23:39:07 -0500

    add hibernate validator to build (#671)
    We finally hit the version number to add this to the regular build

--- http-hibernate-validator/src/main/java/io/avaje/http/hibernate/validator/ValidatorProvider.java
+++ http-hibernate-validator/src/main/java/io/avaje/http/hibernate/validator/ValidatorProvider.java
@@ -2,19 +2,26 @@ package io.avaje.http.hibernate.validator;
 
 import io.avaje.http.api.Validator;
 import io.avaje.inject.BeanScopeBuilder;
+import io.avaje.inject.spi.PluginProvides;
+import jakarta.validation.Validation;
 
-/**
- * Plugin for avaje inject that provides a default BeanValidator instance.
- */
+/** Plugin for avaje inject that provides a default BeanValidator instance. */
+@PluginProvides(Validator.class)
 public final class ValidatorProvider implements io.avaje.inject.spi.InjectPlugin {
 
   @Override
-  public Class<?>[] provides() {
-    return new Class<?>[]{Validator.class};
-  }
-
-  @Override
   public void apply(BeanScopeBuilder builder) {
-    builder.provideDefault(null, Validator.class, BeanValidator::new);
+    builder.provideDefault(
+        Validator.class,
+        () -> {
+          var validator = new BeanValidator();
+          builder.addPostConstruct(
+              b ->
+                  validator.postConstruct(
+                      b.getOptional(jakarta.validation.Validator.class)
+                          .orElseGet(
+                              () -> Validation.buildDefaultValidatorFactory().getValidator())));
+          return validator;
+        });
   }
 }
