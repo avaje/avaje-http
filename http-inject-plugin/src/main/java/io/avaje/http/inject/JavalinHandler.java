244de003f305ca0fabe0b65bb237ac7c68ce7b59
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2026-02-25 15:11:39 -0500

    javalin 7

--- http-inject-plugin/src/main/java/io/avaje/http/inject/JavalinHandler.java
+++ http-inject-plugin/src/main/java/io/avaje/http/inject/JavalinHandler.java
@@ -5,14 +5,14 @@ import java.io.UncheckedIOException;
 
 import io.avaje.http.api.AvajeJavalinPlugin;
 import io.avaje.http.api.ValidationException;
-import io.javalin.config.JavalinConfig;
+import io.javalin.config.JavalinState;
 import io.javalin.http.Context;
 
 final class JavalinHandler extends AvajeJavalinPlugin {
 
   @Override
-  public void onStart(JavalinConfig config) {
-    config.router.mount(r -> r.exception(ValidationException.class, this::handler));
+  public void onStart(JavalinState config) {
+    config.routes.exception(ValidationException.class, this::handler);
   }
 
   private void handler(ValidationException ex, Context ctx) {
