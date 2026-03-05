ea260ea0f7da362e01d2af1109bbb760f61e1c68
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2026-03-05 00:20:45 -0500

    Fix Valid TypeUse Detection

--- http-generator-core/src/main/java/io/avaje/http/generator/core/ValidPrism.java
+++ http-generator-core/src/main/java/io/avaje/http/generator/core/ValidPrism.java
@@ -1,8 +1,11 @@
 package io.avaje.http.generator.core;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.type.TypeKind;
 
 import io.avaje.prism.GeneratePrism;
 
@@ -11,9 +14,9 @@ import io.avaje.prism.GeneratePrism;
     name = "AvajeValidPrism",
     superInterfaces = ValidPrism.class)
 @GeneratePrism(
-  value = javax.validation.Valid.class,
-  name = "JavaxValidPrism",
-  superInterfaces = ValidPrism.class)
+    value = javax.validation.Valid.class,
+    name = "JavaxValidPrism",
+    superInterfaces = ValidPrism.class)
 @GeneratePrism(
     value = jakarta.validation.Valid.class,
     name = "JakartaValidPrism",
@@ -25,17 +28,41 @@ import io.avaje.prism.GeneratePrism;
 public interface ValidPrism {
 
   static Optional<ValidPrism> getOptionalOn(Element e) {
+
     return Optional.<ValidPrism>empty()
         .or(() -> AvajeValidPrism.getOptionalOn(e))
         .or(() -> HttpValidPrism.getOptionalOn(e))
         .or(() -> JakartaValidPrism.getOptionalOn(e))
-        .or(() -> JavaxValidPrism.getOptionalOn(e));
+        .or(() -> JavaxValidPrism.getOptionalOn(e))
+        .or(() -> typeUse(e));
   }
 
   static boolean isPresent(Element e) {
     return AvajeValidPrism.isPresent(e)
         || JakartaValidPrism.isPresent(e)
         || JavaxValidPrism.isPresent(e)
-        || HttpValidPrism.isPresent(e);
+        || HttpValidPrism.isPresent(e)
+        || HttpValidPrism.isPresent(e)
+        || typeUse(e).isPresent();
+  }
+
+  static Optional<ValidPrism> typeUse(Element e) {
+    if (e instanceof ExecutableElement) {
+      ExecutableElement ee = (ExecutableElement) e;
+      var returnType = ee.getReturnType();
+      if (returnType.getKind() != TypeKind.VOID) {
+        return returnType.getAnnotationMirrors().stream()
+            .map(
+                m ->
+                    Optional.<ValidPrism>empty()
+                        .or(() -> AvajeValidPrism.getOptional(m))
+                        .or(() -> JakartaValidPrism.getOptional(m))
+                        .or(() -> JavaxValidPrism.getOptional(m)))
+            .filter(Optional::isPresent)
+            .findFirst()
+            .flatMap(Function.identity());
+      }
+    }
+    return Optional.empty();
   }
 }
