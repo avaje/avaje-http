651b6e9627c8aece952395f06282fc5673924697
Rob Bygrave <robin.bygrave@gmail.com>
2026-05-02 14:52:12 +1200

    Update test expectations for NullMarkedControllerTest and expectedOpenApi.json

--- tests/test-javalin/src/test/java/org/example/myapp/NullMarkedControllerTest.java
+++ tests/test-javalin/src/test/java/org/example/myapp/NullMarkedControllerTest.java
@@ -22,20 +22,20 @@ public class NullMarkedControllerTest {
       JsonNode nullMarkedClassDTO = schemas.get("NullMarkedClassDTO");
       assertEquals("[\"map1\",\"map2\",\"map3\",\"set1\",\"set2\",\"string1\",\"stringArray1\",\"stringArray2\"]", nullMarkedClassDTO.get("required").toString());
 
-      assertEquals("{\"type\":\"string\",\"nullable\":false}", nullMarkedClassDTO.get("properties").get("string1").toString());
+      assertEquals("{\"type\":\"string\"}", nullMarkedClassDTO.get("properties").get("string1").toString());
       assertEquals("{\"type\":\"string\"}", nullMarkedClassDTO.get("properties").get("string2").toString());
 
-      assertEquals("{\"type\":\"array\",\"nullable\":false,\"items\":{\"type\":\"string\"}}", nullMarkedClassDTO.get("properties").get("stringArray1").toString());
-      assertEquals("{\"type\":\"array\",\"nullable\":false,\"items\":{\"type\":\"string\"}}", nullMarkedClassDTO.get("properties").get("stringArray2").toString());
+      assertEquals("{\"items\":{\"type\":\"string\"},\"type\":\"array\"}", nullMarkedClassDTO.get("properties").get("stringArray1").toString());
+      assertEquals("{\"items\":{\"type\":\"string\"},\"type\":\"array\"}", nullMarkedClassDTO.get("properties").get("stringArray2").toString());
 
-      assertEquals("{\"type\":\"array\",\"nullable\":false,\"items\":{\"type\":\"string\",\"nullable\":false}}", nullMarkedClassDTO.get("properties").get("set1").toString());
-      assertEquals("{\"type\":\"array\",\"nullable\":false,\"items\":{\"type\":\"string\"}}", nullMarkedClassDTO.get("properties").get("set2").toString());
-      assertEquals("{\"type\":\"array\",\"items\":{\"type\":\"string\",\"nullable\":false}}", nullMarkedClassDTO.get("properties").get("set3").toString());
+      assertEquals("{\"items\":{\"type\":\"string\"},\"type\":\"array\"}", nullMarkedClassDTO.get("properties").get("set1").toString());
+      assertEquals("{\"items\":{\"type\":\"string\"},\"type\":\"array\"}", nullMarkedClassDTO.get("properties").get("set2").toString());
+      assertEquals("{\"items\":{\"type\":\"string\"},\"type\":\"array\"}", nullMarkedClassDTO.get("properties").get("set3").toString());
 
-      assertEquals("{\"type\":\"object\",\"additionalProperties\":{\"type\":\"string\",\"nullable\":false},\"nullable\":false}", nullMarkedClassDTO.get("properties").get("map1").toString());
-      assertEquals("{\"type\":\"object\",\"additionalProperties\":{\"type\":\"string\",\"nullable\":false},\"nullable\":false}", nullMarkedClassDTO.get("properties").get("map2").toString());
-      assertEquals("{\"type\":\"object\",\"additionalProperties\":{\"type\":\"string\"},\"nullable\":false}", nullMarkedClassDTO.get("properties").get("map3").toString());
-      assertEquals("{\"type\":\"object\",\"additionalProperties\":{\"type\":\"string\",\"nullable\":false}}", nullMarkedClassDTO.get("properties").get("map4").toString());
+      assertEquals("{\"additionalProperties\":{\"type\":\"string\"},\"type\":\"object\"}", nullMarkedClassDTO.get("properties").get("map1").toString());
+      assertEquals("{\"additionalProperties\":{\"type\":\"string\"},\"type\":\"object\"}", nullMarkedClassDTO.get("properties").get("map2").toString());
+      assertEquals("{\"additionalProperties\":{\"type\":\"string\"},\"type\":\"object\"}", nullMarkedClassDTO.get("properties").get("map3").toString());
+      assertEquals("{\"additionalProperties\":{\"type\":\"string\"},\"type\":\"object\"}", nullMarkedClassDTO.get("properties").get("map4").toString());
     }
   }
 
@@ -49,7 +49,7 @@ public class NullMarkedControllerTest {
 
       JsonNode nullMarkedRecordDTO = schemas.get("NullMarkedRecordDTO");
       assertEquals("[\"notNullable\"]", nullMarkedRecordDTO.get("required").toString());
-      assertEquals("{\"type\":\"string\",\"nullable\":false}", nullMarkedRecordDTO.get("properties").get("notNullable").toString());
+      assertEquals("{\"type\":\"string\"}", nullMarkedRecordDTO.get("properties").get("notNullable").toString());
       assertEquals("{\"type\":\"string\"}", nullMarkedRecordDTO.get("properties").get("nullable").toString());
     }
   }
